3d6d0e03a9b518535edc1382a80ac8539e6bfbad
Alexis12vloq <valujano@unac.edu.pe>
2025-06-16 03:00:41 -0500

    Lazy Loading Feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,43 @@
 import { Routes } from '@angular/router';
-import { PlanMediosCreate } from './plan-medios/plan-medios-create/plan-medios-create';
-import { PlanMediosResumen } from './plan-medios/plan-medios-resumen/plan-medios-resumen';
-import { PlanMediosConsulta } from './plan-medios/plan-medios-consulta/plan-medios-consulta';
-import { PlanMediosNuevaPauta } from './plan-medios/plan-medios-nueva-pauta/plan-medios-nueva-pauta';
-import { Reglas } from './reglas/reglas';
-import { Configuracion } from './configuracion/configuracion';
 
 export const routes: Routes = [
-  { path: 'plan-medios-create', component: PlanMediosCreate },
-  { path: 'plan-medios-resumen', component: PlanMediosResumen },
-  { path: 'plan-medios-consulta', component: PlanMediosConsulta },
-  { path: 'plan-medios-nueva-pauta', component: PlanMediosNuevaPauta },
-  { path: 'reglas', component: Reglas },
-  { path: 'configuracion', component: Configuracion },
+  {
+    path: 'plan-medios-create',
+    loadComponent: () =>
+      import('./plan-medios/plan-medios-create/plan-medios-create').then(
+        (m) => m.PlanMediosCreate
+      ),
+  },
+  {
+    path: 'plan-medios-resumen',
+    loadComponent: () =>
+      import('./plan-medios/plan-medios-resumen/plan-medios-resumen').then(
+        (m) => m.PlanMediosResumen
+      ),
+  },
+  {
+    path: 'plan-medios-consulta',
+    loadComponent: () =>
+      import('./plan-medios/plan-medios-consulta/plan-medios-consulta').then(
+        (m) => m.PlanMediosConsulta
+      ),
+  },
+  {
+    path: 'plan-medios-nueva-pauta',
+    loadComponent: () =>
+      import('./plan-medios/plan-medios-nueva-pauta/plan-medios-nueva-pauta').then(
+        (m) => m.PlanMediosNuevaPauta
+      ),
+  },
+  {
+    path: 'reglas',
+    loadComponent: () => import('./reglas/reglas').then((m) => m.Reglas),
+  },
+  {
+    path: 'configuracion',
+    loadComponent: () =>
+      import('./configuracion/configuracion').then((m) => m.Configuracion),
+  },
   { path: '', redirectTo: '/reglas', pathMatch: 'full' },
-  { path: '**', redirectTo: '', pathMatch: 'full' }
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
