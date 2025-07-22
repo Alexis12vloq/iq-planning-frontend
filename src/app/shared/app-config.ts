35d6924ed582a2c8902f761177ade2c98e8f9cb1
rarrascue <MG6449-A@engie.com>
2025-07-22 10:39:55 -0500

    feat: Actualizar la configuración de la API en app-config.ts, cambiando la URL de localhost a la de Azure. Se comenta la URL de localhost para facilitar la transición al entorno de producción.

--- src/app/shared/app-config.ts
+++ src/app/shared/app-config.ts
@@ -7,6 +7,6 @@ export interface AppConfig {
 export const APP_CONFIG = new InjectionToken<AppConfig>('app.config');
 
 export const APP_CONFIG_VALUE: AppConfig = {
-  apiUrl: 'https://localhost:7223' // Configurado para localhost puerto 7223
-  //apiUrl: 'https://iq-planningapi20250714104214-dnebf8aqhnerdjba.mexicocentral-01.azurewebsites.net' // Azure (comentado)
+  //apiUrl: 'https://localhost:7223' // Configurado para localhost puerto 7223
+  apiUrl: 'https://iq-planningapi20250714104214-dnebf8aqhnerdjba.mexicocentral-01.azurewebsites.net' // Azure (comentado)
 };
